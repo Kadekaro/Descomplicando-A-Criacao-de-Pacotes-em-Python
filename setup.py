043f762715df7fd2408a12db85d6d305630a3a76
Kadekaro <38010624+Kadekaro@users.noreply.github.com>
2023-08-26 23:07:31 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="Image-Processing",
+    name="Image-Processing-Testando-O-Primeiro",
     version="0.0.1",
     author="Wesley Kadekaro",
     author_email="kadekaroshop@gmail.com",
     description="Aprendendo a fazer a criação de pacotes em Python",
     long_description=page_description,
-    long_description_content_type=requirements,
+    long_description_content_type="text/markdown",
     url="https://github.com/Kadekaro/Descomplicando-A-Criacao-de-Pacotes-em-Python/tree/master",
     packages=find_packages(),
     install_requires=requirements,
